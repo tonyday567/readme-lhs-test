e90f1cd558928fd3457d49fe6815ebf9f168b9a3
Tony Day <tonyday567@gmail.com>
2020-06-28 12:03:32 +1000

    new version

---
+++ app/readme-lhs-test.hs
@@ -0,0 +1,21 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedLabels #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE RebindableSyntax #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
+{-# OPTIONS_GHC -Wall #-}
+
+import NumHask.Prelude
+
+main :: IO ()
+main = do
+  let n = 10
+  let answer = 1 + product [1..n::Integer]
+  print answer
